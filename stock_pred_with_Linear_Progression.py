6197cab1fe050f042e675048772fd1d0bd7003fd
Myst-Lee <88233357+Myst-Lee@users.noreply.github.com>
2022-12-05 02:52:06 +0800

    Update stock_pred_with_Linear_Progression.py

--- stock_pred_with_Linear_Progression.py
+++ stock_pred_with_Linear_Progression.py
@@ -273,6 +273,8 @@ elif sb =='Sell Stock':
         
 #         template.loc[len(template.index)] = presentable_data.to_numpy()
 #         template = pd.concat([template, presentable_data])
+
+        st.write(template[0])
         st.write(template)
 
 #         template = template.append(presentable_data)
