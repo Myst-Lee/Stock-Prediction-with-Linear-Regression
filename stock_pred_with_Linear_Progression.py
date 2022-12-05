7bfcb39f55d611d688a6780b01f9ec5919de461a
Myst-Lee <88233357+Myst-Lee@users.noreply.github.com>
2022-12-05 13:12:30 +0800

    Update stock_pred_with_Linear_Progression.py

--- stock_pred_with_Linear_Progression.py
+++ stock_pred_with_Linear_Progression.py
@@ -271,7 +271,10 @@ elif sb =='Sell Stock':
 
         strategy = presentable_data.iloc[0]['Sell/Hold']
         st.subheader("Strategy (Hold/Sell): "+ strategy)
-        template = template.append(presentable_data)
+        test = np.DataFrame(st.secrets.dataframe)
+        test = template.append(presentable_data)
+        st.write(test)
+        
 
 #         template = template.append(presentable_data)
 
