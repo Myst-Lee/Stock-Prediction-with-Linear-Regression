c4cb60dbfc528816162c14366558900a8a4ff1e8
Myst-Lee <88233357+Myst-Lee@users.noreply.github.com>
2022-12-05 18:44:07 +0800

    Update stock_pred_with_Linear_Progression.py

--- stock_pred_with_Linear_Progression.py
+++ stock_pred_with_Linear_Progression.py
@@ -464,7 +464,7 @@ else:
     @st.cache
     def expensive_computation(a, b):
         time.sleep(2)  # This makes the function take 2s to run
-    return a * b
+        return a * b
 
     a = 2
     b = 21
